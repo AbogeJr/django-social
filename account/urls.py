a7379f54888d875a12318ed1a6a1c10793532524
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-05-19 19:13:59 +0300

    added registration

--- account/urls.py
+++ account/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     # change password urls
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
     # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
